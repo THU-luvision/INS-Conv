fd8ba9969c402aa0cdb2f3adafb5757ce91a6951
liuleyao <407553429@qq.com>
2022-09-26 11:39:47 +0800

    add demo

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-from xml.sax.handler import feature_external_ges
 import torch
 import copy
 import numpy as np
@@ -12,7 +11,6 @@ import sparseconvnet as scn
 import model
 import sys
 
-sys.path.append('')
 
 config = {'dimension': 3, 'full_scale': 4096}
 
@@ -20,10 +18,24 @@ Model = model.Naive_UNet(config)
 
 Model = Model.cuda()
 
-points = torch.randint(1000, 2000, size=(10000, 3)).cuda()     # initial point xyz
-features = torch.rand((10000, 3)).cuda()                      # initial point features
+points_0 = torch.randint(100, 150, size=(10000, 3)).cuda()     # frame 0 point xyz
+features_0 = torch.rand((10000, 3)).cuda()                      # frame 0 point features
+
+# the initial update of the network, use inccrement=False
+# this will save a checkpoint of network feataures, do it every 100 frames.
+output_0 = Model([points_0, features_0], increment=False)    
 
 
-output = Model([points, features], increment=False) 
+points_1 = torch.randint(100, 150, size=(1000, 3)).cuda()     # xyz of incremental points of frame 1
+features_1 = torch.rand((10000, 3)).cuda()                      # features of incremental points of frame 1
 
 
+# incremental update of the network, use inccrement=True
+# just need to input the points and features, the input residuals will be computed automaticlly
+# the output is the results of new points in frame 1
+output_1 = Model([points_1, features_1], increment=True)    
+
+'''
+    ...
+    more frame
+'''
